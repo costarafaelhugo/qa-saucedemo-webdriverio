82133c4dc7471a50ce7221c6315f63489fb9320d
Hugo Costa <hugo.costa@omni.chat>
2025-09-01 10:40:35 -0300

    login invalid check

--- tests/specs/login.invalid.spec.js
+++ tests/specs/login.invalid.spec.js
@@ -1,35 +1,46 @@
 const LoginPage = require('../pageobjects/login.page');
 
 describe('Login - Credenciais Inválidas', () => {
+
     it('Deve exibir erro ao usar senha incorreta', async () => {
-        await LoginPage.open();
+        await LoginPage.open('');
         await LoginPage.login('standard_user', 'senha_errada');
 
         await LoginPage.errorMessage.waitForDisplayed();
-        await expect(LoginPage.errorMessage).toHaveTextContaining(
-            'Epic sadface: Username and password do not match any user in this service'
+        await expect(LoginPage.errorMessage).toHaveText(
+            expect.stringContaining('Epic sadface: Username and password do not match any user in this service')
+        );
+    });
+
+    it('Deve exibir erro ao usar usuário incorreto', async () => {
+        await LoginPage.open('');
+        await LoginPage.login('standar', 'secret_sauce');
+
+        await LoginPage.errorMessage.waitForDisplayed();
+        await expect(LoginPage.errorMessage).toHaveText(
+            expect.stringContaining('Epic sadface: Username and password do not match any user in this service')
         );
     });
 
     it('Deve exibir erro ao deixar campo de usuário vazio', async () => {
-        await LoginPage.open();
+        await LoginPage.open('');
         await LoginPage.inputPassword.setValue('secret_sauce');
         await LoginPage.btnLogin.click();
 
         await LoginPage.errorMessage.waitForDisplayed();
-        await expect(LoginPage.errorMessage).toHaveTextContaining(
-            'Epic sadface: Username is required'
+        await expect(LoginPage.errorMessage).toHaveText(
+            expect.stringContaining('Epic sadface: Username is required')
         );
     });
 
     it('Deve exibir erro ao deixar campo de senha vazio', async () => {
-        await LoginPage.open();
+        await LoginPage.open('');
         await LoginPage.inputUsername.setValue('standard_user');
         await LoginPage.btnLogin.click();
 
         await LoginPage.errorMessage.waitForDisplayed();
-        await expect(LoginPage.errorMessage).toHaveTextContaining(
-            'Epic sadface: Password is required'
+        await expect(LoginPage.errorMessage).toHaveText(
+            expect.stringContaining('Epic sadface: Password is required')
         );
     });
 });
